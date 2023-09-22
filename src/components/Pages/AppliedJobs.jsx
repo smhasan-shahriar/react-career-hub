ecd95ce44f3b26ecc8f54358923aedcb95d2d65b
Ador Shahriar <ador.shahriar@gmail.com>
2023-09-22 13:59:52 +0600

    applied jobs retrieved from local storage section

--- src/components/Pages/AppliedJobs.jsx
+++ src/components/Pages/AppliedJobs.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getJobsFromStorage } from '../../utilities/localStorage';
 
 const AppliedJobs = () => {
+    const [appliedJobs, setAppliedJobs] = useState([])
+    useEffect(()=> {
+        const fetchFunction = async () =>{
+            const response = await fetch('jobs.json');
+            const data = await response.json();
+            setAppliedJobs(data);
+            const jobs = [...data]; 
+            const selectedJobs = getJobsFromStorage();
+            const jobsApplied = jobs.filter(job => selectedJobs.includes(job.id))
+            setAppliedJobs(jobsApplied)
+        }
+        fetchFunction();
+    },[])
     return (
         <div>
-            applied jobs
+            <div className='linear-bg h-[300px] flex items-center justify-center'>
+                <h3 className='text-[32px] font-extrabold'>Applied Jobs</h3>
+            </div>
         </div>
     );
 };
